20380a2bc590c37564c9128fee88dc876c9e74ef
Jonnathan23 <jsaquicela26@gmail.com>
2025-11-27 22:36:13 -0500

    creacion de proyectos

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -9,7 +9,7 @@ export default function MyProyects() {
             <div>
                 <ProyectDescription
                     linkGitHub="https://github.com/Jonnathan23/uptask-frontend"
-                    linkImage="img/uptask.png"
+                    linkImage="img/uptask_MERN.png"
                     title="UpTask"
                 >
                     <p>UpTask es una aplicación para administrar tus tareas diarias.</p>
@@ -21,7 +21,10 @@ export default function MyProyects() {
                     linkImage="img/cocktail.png"
                     title="Cocktail"
                 >
-                    <p><strong>Proyecto de recetas de bebidas</strong> en este proyecto se utilizó la API de <a href="https://www.thecocktaildb.com/" target="blank">CocktailDB</a></p>
+                    <p>
+                        <strong>Proyecto de recetas de bebidas</strong>
+                        en este proyecto se utilizó la API de <a href="https://www.thecocktaildb.com/" target="blank">CocktailDB</a>
+                    </p>
                 </ProyectDescription>
 
                 <ProyectDescription
@@ -36,24 +39,26 @@ export default function MyProyects() {
                 <ProyectDescription
                     linkWeb="https://control-gastos-react-js.netlify.app/"
                     linkGitHub="https://github.com/Jonnathan23/control-gastos"
-                    linkImage="img/control_gastos.png"
+                    linkImage="img/control_gastos_miniatura_frontend.png"
                     title="Control Gastos"
                 >
-                    <p><strong>Proyecto de seguimiento de pacientes</strong> en este proyecto se demuestra el uso de Zustand</p>
+                    <p><strong>Proyecto de control de gastos</strong> el proyecto consiste en una aplicación para controlar tus gastos en base a categorías de gastos, actualmente se está expandiendo el proyecto</p>
                 </ProyectDescription>
 
                 <ProyectDescription
                     linkGitHub="https://github.com/Jonnathan23/backend-ups-task"
+                    linkImage="img/uptask_backend.png"
                     title="Uptask Backend"
                 >
-                    <p><strong>Proyecto Backend en Node</strong> en este proyecto se demuestra el uso de Node.js y Express y uso de ORNM</p>
+                    <p><strong>Proyecto Backend en Node</strong> en este proyecto se demuestra el uso de Node.js y Express y uso de ODNM</p>
                 </ProyectDescription>
 
                 <ProyectDescription
                     linkGitHub="https://github.com/Jonnathan23/Rest_api_node_ts_server"
+                    linkImage="img/control_gastos_miniatura.png"
                     title="Rest API"
                 >
-                    <p><strong>Proyecto de REST API</strong> en este proyecto se demuestra el uso de Node.js y Express con testing</p>
+                    <p><strong>Proyecto de REST API con PERN</strong> en este proyecto se demuestra el uso de Node.js y Express con testing</p>
                 </ProyectDescription>
             </div>
         </section >
---
+++ src/components/MySkills.tsx
@@ -0,0 +1,99 @@
+
+export default function MySkills() {
+    return (
+        <section>
+            <h2>Mis Habilidades</h2>
+            <p>Como programador Full Stack, tengo experiencia trabajando con un ecosistema de tecnologías que cubren todo el ciclo de desarrollo.</p>
+
+            <div className="skills-container">
+                <div className="skill-category">
+                    <h3>Frontend</h3>
+                    <ul>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div className="devicon-react-original-wordmark colored">
+
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
+
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactnative/reactnative-original-wordmark.svg" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="skill-category">
+                    <h3>Backend</h3>
+                    <ul>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="skill-category">
+                    <h3>Bases de Datos</h3>
+                    <ul>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-plain-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="skill-category">
+                    <h3>Herramientas y Control de Versiones</h3>
+                    <ul>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <img width="50px" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bitbucket/bitbucket-original-wordmark.svg" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+}
